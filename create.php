8576d318884f43d4b5c8894ccd573ea5551bc8ab
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-08-30 12:07:21 -0300

    UPDATE - Mudanças visuais no create

--- create.php
+++ create.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "Erro" . $sql . "<br>" . $conn->error;
             $conn->close();
         };
-    }
+    };
     if (isset($_POST['addProfessor'])) {
         $nome = $_POST['nome'];
         $email = $_POST['email'];
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "Erro" . $sql . "<br>" . $conn->error;
             $conn->close();
         };
-    }
+    };
     if (isset($_POST['addAluno'])) {
         $nome = $_POST['nome'];
         $fk_turma = $_POST['fk_turma'];
@@ -43,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         if ($conn->query($sql) === true) {
             echo "Registro Adicionado";
+            header('Location: read.php');
             $conn->close();
             exit();
         } else {
@@ -79,47 +80,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
-<body style="padding: 5px; display: flex; gap: 100px;">
+<body style="padding: 5px; display: flex; gap: 100px; justify-content:center;">
     <form method="POST">
         <label for="turma">
             <h2>Adicionar turma:</h2>
         </label><br>
-        <input type="text" name="nome" placeholder="Nome da Turma" style="color: black;"><br>
-        <input type="text" name="serie" placeholder="Série da Turma" style="color: black; placeholder-opacity-max:0.5;"><br>
-        <button type="submit" class="btn btn-secondary" name="addTurma">Enviar</button>
+        <input type="text" name="nome" placeholder="Nome da Turma" style="color: black; width: 100%;"><br>
+        <input type="text" name="serie" placeholder="Série da Turma" style="color: black; width: 100%;"><br>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-secondary" name="addTurma">Enviar</button>
+        </div>
     </form>
     <form method="POST">
         <label for="turma">
             <h2>Adicionar professor:</h2>
         </label><br>
-        <input type="text" name="nome" placeholder="Nome do Professor" style="color: black;"><br>
-        <input type="email" name="email" placeholder="Email do Professor" style="color: black;"><br>
-        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black;"><br>
-        <select name="materia" class="form-select" multiple aria-label="Multiple select example" style="width: 70%;">
+        <input type="text" name="nome" placeholder="Nome do Professor" style="color: black; width: 100%;"><br>
+        <input type="email" name="email" placeholder="Email do Professor" style="color: black; width: 100%;"><br>
+        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black; width: 100%;"><br>
+        <select name="materia" class="form-select" multiple aria-label="Multiple select example" style="width: 100%;">
             <option selected>Matérias</option>
             <option value="ciencias">Ciências</option>
             <option value="matematica">Matemática</option>
             <option value="historia">História</option>
             <option value="portugues">Português</option>
         </select><br>
-        <button type="submit" class="btn btn-secondary" name="addProfessor">Enviar</button>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-secondary" name="addProfessor">Enviar</button>
+        </div>
     </form>
     <form method="POST">
         <label for="turma">
             <h2>Adicionar aluno:</h2>
         </label><br>
-        <input type="text" name="nome" placeholder="Nome da Turma" style="color: black;"><br>
-        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black;"><br>
-        <button type="submit" class="btn btn-secondary" name="addAluno">Enviar</button>
+        <input type="text" name="nome" placeholder="Nome da Turma" style="color: black; width: 100%;"><br>
+        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black; width: 100%;"><br>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-secondary" name="addAluno">Enviar</button>
+        </div>
     </form>
     <form method="POST">
         <label for="atividade">
             <h2>Adicionar atividade:</h2>
         </label><br>
-        <input type="text" name="descricao" placeholder="Descrição" style="color: black;"><br>
-        <input type="text" name="fk_professor" placeholder="Professor (ID)" style="color: black;"><br>
-        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black;"><br>
-        <button type="submit" class="btn btn-secondary" name="addAtividade">Enviar</button>
+        <input type="text" name="descricao" placeholder="Descrição" style="color: black; width: 100%;"><br>
+        <input type="text" name="fk_professor" placeholder="Professor (ID)" style="color: black; width: 100%;"><br>
+        <input type="text" name="fk_turma" placeholder="Turma (ID)" style="color: black; width: 100%;"><br>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-secondary" name="addAtividade">Enviar</button>
+        </div>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
---
+++ read.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "<button name=''";
+
+?>
